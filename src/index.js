85585d8e6b4b322f605a87204c467f7a7f2006b0
Bondok6 <kirolousbondok6@gmail.com>
2022-01-14 00:34:04 +0200

    Remove displayAllTasks function, and Use render function instead

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import "./style.css";
 import displayAllTasks from "./modules/displayAllTasks.js";
 import addTask from "./modules/addTask.js";
 import options from "./modules/allOptions.js";
+import render from "./modules/render";
 
 document.addEventListener("keypress", addTask);
 document.addEventListener("click", options);
 
-displayAllTasks();
+render();
