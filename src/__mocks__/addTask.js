f4d9d53000ca8421d9a917c3cd3683c8436099e9
Bondok6 <kirolousbondok6@gmail.com>
2022-01-20 01:51:05 +0200

    Add __mock__s and __test__ folders || Make addTask.js pure function and Implement the test

---
+++ src/__mocks__/addTask.js
@@ -0,0 +1,20 @@
+import Tasks from "../modules/classTasks";
+
+const addTask = (input) => {
+  // check If the value not empty
+  if (input.trim() === "") return;
+
+  // check if there is no repetition
+  const t = Tasks.tasks.filter((task) => input === task.description);
+  if (t.length) return;
+
+  // check if there is no white spaces
+  if (input.startsWith(" ") || input.endsWith(" ")) input = input.trim();
+
+  let id = Tasks.tasks.length + 1;
+
+  const newTask = new Tasks(input, id);
+  Tasks.tasks.push(newTask);
+};
+
+export default addTask;
