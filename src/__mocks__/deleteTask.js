129054737f10f2468d67ac7aafc202a2bb016f90
Bondok6 <kirolousbondok6@gmail.com>
2022-01-20 17:16:18 +0200

    Add deleteTask.test.js

--- src/__mocks__/deleteTask.js
+++ src/__mocks__/deleteTask.js
@@ -1,5 +1,7 @@
 import Tasks from "./../modules/classTasks";
+
 const deleteTask = (input) => {
+  if (Tasks.tasks.length === 0) return false;
   Tasks.tasks = Tasks.tasks.filter((task) => task.description !== input);
   Tasks.tasks.pop();
 };
---
+++ src/__test__/deleteTask.test.js
@@ -0,0 +1,20 @@
+import addTask from "../__mocks__/addTask.js";
+import Tasks from "./../modules/classTasks";
+import deleteTask from "../__mocks__/deleteTask";
+
+describe("Remove task in the list", () => {
+  test("Remove the target task", () => {
+    const input = "task10";
+    addTask(input); // Add Task
+
+    deleteTask(input); // Remove Task
+
+    deleteTask("task 1"); // Remove another Task
+
+    expect(Tasks.tasks).toHaveLength(0);
+  });
+
+  test("Test if the array is empty", () => {
+    expect(Tasks.tasks.length).toBeFalsy();
+  });
+});
